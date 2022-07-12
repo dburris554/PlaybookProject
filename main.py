bd6a9f4871c80df3f5fddb24c8a3d6c9f7a1664d
dburris554 <dburris554@yahoo.com>
2022-07-11 22:43:49 -0500

    Files from local

---
+++ main.py
@@ -0,0 +1,34 @@
+from example2 import ChopperScape
+
+env = ChopperScape()
+obs = env.reset()
+
+while True:
+    action = env.action_space.sample()
+    obs, reward, done, _ = env.step(action)
+    env.render()
+
+    if done:
+        break
+
+env.close()
+
+from stable_baselines3 import PPO
+from stable_baselines3.ppo import MlpPolicy
+
+# import gym
+
+# env = gym.make("CartPole-v1")
+# expert = PPO(
+#     policy=MlpPolicy,
+#     env=env,
+#     seed=0,
+#     batch_size=64,
+#     ent_coef=0.0,
+#     learning_rate=0.0003,
+#     n_epochs=10,
+#     n_steps=64,
+#     verbose=1
+# )
+# expert.learn(1000)  # Note: set to 100000 to train a proficient expert
+# print(expert)
