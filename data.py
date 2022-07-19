e0ee1c4af7a204f9d40a5d560b06bbb762f35ba1
OctoSpin <62153805+Octo-Spin@users.noreply.github.com>
2022-07-18 22:46:56 -0500

    Started dataframe

---
+++ data.py
@@ -0,0 +1,9 @@
+import pandas as pd
+import numpy as np 
+
+# define DataFrame
+df = pd.DataFrame(columns=['Missile Count', 'Formation Used', 'Turrent Count','Missiles Survive'],dtype=int)
+# What more data do we want?
+# How to pull results from colloseum tests?
+# Will do more once I can figure out lines 6 and 7.
+# I think collecting data from colloseum.py as series might work best, but not sure at the moment.
