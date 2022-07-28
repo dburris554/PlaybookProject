9d0c9efe55ba10137e6001d30a36f5ee9cbc784e
dburris554 <dburris554@yahoo.com>
2022-07-27 23:44:59 -0500

    Small rl alg argument changes

--- rl_main.py
+++ rl_main.py
@@ -1,10 +1,10 @@
 from learning import RL_Env
 
-print("This are random actions in the given environment.")
+print("-------------These are random actions in the given environment-------------")
 env = RL_Env()
 episodes = 0
 obs = env.reset()
-while episodes < 2:
+while episodes < 5:
     action = env.action_space.sample()
     obs, reward, done, info = env.step(action)
     env.render(mode="human")
@@ -15,7 +15,7 @@ while episodes < 2:
 
 env.close()
 
-print("Then an RL algorithm is trained in this environment")
+print("-------------Then an RL algorithm is trained in this environment-------------")
 from stable_baselines3 import PPO
 from stable_baselines3.ppo import MlpPolicy
 
@@ -23,20 +23,20 @@ expert = PPO(
     policy=MlpPolicy,
     env=RL_Env(),
     seed=0,
-    batch_size=64,
+    batch_size=32,
     ent_coef=0.0,
-    learning_rate=0.01, # Usually something like 0.003
+    learning_rate=0.03, # Note: usually 0.003
     n_epochs=10,
-    n_steps=64,
+    n_steps=32,
     verbose=1
 )
 expert = expert.learn(200)  # Note: set to 100000 to train a proficient expert
 
-print("After training, this is how the algorithm acts in the environment")
+print("-------------This is how the trained algorithm acts in the environment-------------")
 env = RL_Env()
 episodes = 0
 obs = env.reset()
-while episodes < 5:
+while episodes < 10:
     action = expert.policy.predict(obs)[0]
     obs, reward, done, info = env.step(action)
     env.render(mode="human")
