51114032bf7bb1fdc07d997170a979021794482a
dburris554 <dburris554@yahoo.com>
2022-07-30 21:07:57 -0500

    Removed dead code from main.py

--- main.py
+++ main.py
@@ -15,23 +15,3 @@ while True:
         break
 
 env.close()
-
-# from stable_baselines3 import PPO
-# from stable_baselines3.ppo import MlpPolicy
-
-# import gym
-
-# env = gym.make("CartPole-v1")
-# expert = PPO(
-#     policy=MlpPolicy,
-#     env=env,
-#     seed=0,
-#     batch_size=64,
-#     ent_coef=0.0,
-#     learning_rate=0.0003,
-#     n_epochs=10,
-#     n_steps=64,
-#     verbose=1
-# )
-# expert.learn(1000)  # Note: set to 100000 to train a proficient expert
-# print(expert)
