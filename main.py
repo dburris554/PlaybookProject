7cd3388f06efd191b92fc0bd85c02fa76796ae51
OctoSpin <62153805+Octo-Spin@users.noreply.github.com>
2022-07-30 23:55:08 -0500

    added DF and save to disk as CSV

--- main.py
+++ main.py
@@ -1,17 +1,30 @@
+import pandas as pd
 from colosseum import FormationEnv
 from formations import *
 
-env = FormationEnv(Marching3, turret_count=2, ep_count=100)
-obs = env.reset()
+pairs = []
+turret = [1,2,3,4]
+formations = [Ant3, Marching3, V3]
+df = pd.DataFrame(columns = ["Missile Count",  "Formation Used",  "Turret Count",  "Missiles Survived"])
+for f in formations:
+    for t in turret:
+        pairs.append((f,t))
+for p in pairs:
+    env = FormationEnv(p[0], turret_count=p[1], ep_count=100)
+    obs = env.reset()
 
-while True:
-    action = 0
-    obs, reward, done, info = env.step(action)
-    env.render(mode="rgb_array")
+    while True:
+        action = 0
+        obs, reward, done, info = env.step(action)
+        #env.render(mode="human")
 
-    if done:
-        data = info["data"]
-        #print(data)
-        break
+        if done:
+            data = info["data"]
+            #print(data)
+            df = df.merge(data, how = "outer")
+            #print(data)
+            break
+
+df.to_csv('D:\School Projects\Project 2\json\hundred_test.csv')
 
 env.close()
