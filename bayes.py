02d583fed425d1b3e079dea3d52c9045c4043754
OctoSpin <62153805+Octo-Spin@users.noreply.github.com>
2022-08-01 22:15:28 -0500

    god help us

--- bayes.py
+++ bayes.py
@@ -3,7 +3,7 @@ import pandas as pd
 from sklearn.naive_bayes import GaussianNB
 from sklearn import metrics
 
-'''df = pd.read_csv('Missile All.csv')
+df = pd.read_csv('Missile All.csv')
 opt = [5]
 flt_df = df[df["Missile Count"].isin(opt)]
 #print(df)
@@ -27,14 +27,12 @@ gnb.fit(X_train, y_train)
 # Predict missile survivability
 y_pred = gnb.predict(X_test)
 
-# GNB Predictions
-print(gnb.classes_)
-predicted = gnb.predict_proba([[5,10,3]])
-print("The gods tell me", predicted, "missiles will survive")
+def oracle(gnb : GaussianNB, n_missiles : int, n_turrets : int) -> str :
+    # GNB Predictions
+    print(gnb.classes_)
+    predicted = gnb.predict_proba([[5,10,3]])
+    print("The gods tell me", predicted, "missiles will survive")
 
-# Testing Accuracy
-print("Accuracy: ",metrics.accuracy_score(y_test, y_pred))'''
-
-n = 8.99284722486562e-02 
-converted_number = float(("%.17f" % n).rstrip('0').rstrip('.'))
-print(round(converted_number*100,2),"%")
+    #n = 8.99284722486562e-02 
+    #converted_number = float(("%.17f" % n).rstrip('0').rstrip('.'))
+    #print(round(converted_number*100,2),"%")
