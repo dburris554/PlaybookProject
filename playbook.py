5f3cd4bf691dc35e3bcf8ae13c53f971e6df5745
OctoSpin <62153805+Octo-Spin@users.noreply.github.com>
2022-08-01 22:14:22 -0500

    it begins

--- playbook.py
+++ playbook.py
@@ -1,12 +1,17 @@
+import pandas as pd
+
 missiles = input("Enter the number of missiles:\n")
 turrets = input("Enter the number of turrets: \n")
 
 print(f"Given {missiles} missiles and {turrets} turrets:\n")
 
 # Use known statistics from Supervised Learning models
-if missiles == 3 and turrets == 4:
-    print("Use this formation")
-elif missiles == 4 and turrets == 2:
+if missiles == 3 and turrets == 4: # TODO pull from bayes model
+    print("V3: 2 missiles surviving with 39% Confidence")
+    print("Ant3: 3 missiles surviving with 20% Confidence")
+    print("Marching3: 2 missiles surviving with 35% Confidence")
+    print("You must choose...wisely")
+elif missiles == 4 and turrets == 2:  # TODO refine
     print("Use that formation")
 else:
     print("Unknown answer")
